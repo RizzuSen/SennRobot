5ae2f1888442bb5778b1763586287be0bb21c639
Kitaroo <95475183+Kitaroo@users.noreply.github.com>
2021-12-08 22:14:10 +0700

    Update approve_sql.py

--- SkyzuRobot/modules/sql/approve_sql.py
+++ SkyzuRobot/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Integer, func, distinct
 
-from SkyzuRobot.modules.sql import BASE, SESSION
+from KyyRobot.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
