11d0649ed8a38d219d852d86075f3920c4c06566
Kitaroo <95475183+Kitaroo@users.noreply.github.com>
2021-12-08 22:06:54 +0700

    Update __init__.py

--- SkyzuRobot/modules/sql/__init__.py
+++ SkyzuRobot/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from SkyzuRobot import DB_URI
+from KyyRobot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
