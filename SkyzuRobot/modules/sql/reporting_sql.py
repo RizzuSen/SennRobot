bb8111f0ed86a682c21bda3d3f84c83fce3d74a1
Kitaroo <95475183+Kitaroo@users.noreply.github.com>
2021-12-08 22:29:03 +0700

    Update reporting_sql.py

--- SkyzuRobot/modules/sql/reporting_sql.py
+++ SkyzuRobot/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from SkyzuRobot.modules.sql import BASE, SESSION
+from KyyRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String
 
 
