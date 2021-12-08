ec12fd47cf2621a65ceb4c3efa4c831be1f43bb4
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-08 20:12:41 +0700

    Update errors.py

--- SkyzuRobot/utils/errors.py
+++ SkyzuRobot/utils/errors.py
@@ -7,7 +7,7 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from SkyzuRobot import LOGGER, pbot as app
+from KyyRobot import LOGGER, pbot as app
 
 
 def split_limits(text):
