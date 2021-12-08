2d8454aa278a008708dd5a19a6dc4fc5b8217ed5
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-08 11:14:29 +0700

    Update sample_config.py

--- SkyzuRobot/sample_config.py
+++ SkyzuRobot/sample_config.py
@@ -18,13 +18,13 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1606221784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "excrybaby"
-    SUPPORT_CHAT = "Skyzusupport"  # Your own group for support, do not add the @
+    OWNER_USERNAME = "IDnyaKosong"
+    SUPPORT_CHAT = "NastySupportt"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001432609692
+        -1001288822269
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001150905176
+        -1001288822269
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
