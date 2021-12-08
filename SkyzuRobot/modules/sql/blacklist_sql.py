76dc6a5dd77c81b283681a401e040085b85450e2
Kitaroo <95475183+Kitaroo@users.noreply.github.com>
2021-12-08 22:16:52 +0700

    Update blacklist_sql.py

--- SkyzuRobot/modules/sql/blacklist_sql.py
+++ SkyzuRobot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from SkyzuRobot.modules.sql import SESSION, BASE
+from KyyRobot.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
