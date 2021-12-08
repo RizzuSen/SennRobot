ed21cc64509c5585dc1f9800b1c233abcb244848
Sayonara <89360980+krisnadiwangga@users.noreply.github.com>
2021-12-08 20:44:02 +0700

    Update anime.py

--- SkyzuRobot/modules/anime.py
+++ SkyzuRobot/modules/anime.py
@@ -5,8 +5,8 @@ import textwrap
 import bs4
 import jikanpy
 import requests
-from SkyzuRobot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
-from SkyzuRobot.modules.disable import DisableAbleCommandHandler
+from KyyRobot import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
+from KyyRobot.modules.disable import DisableAbleCommandHandler
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
                       Update)
 from telegram.ext import CallbackContext, CallbackQueryHandler
