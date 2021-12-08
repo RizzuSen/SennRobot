110758c2007a0e7e0c4ab0d006abbeafa625e7fb
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-08 20:11:26 +0700

    Update http.py

--- SkyzuRobot/utils/http.py
+++ SkyzuRobot/utils/http.py
@@ -19,7 +19,7 @@ SOFTWARE.
 """
 from asyncio import gather
 
-from SkyzuRobot import aiohttpsession as session
+from KyyRobot import aiohttpsession as session
 
 
 async def get(url: str, *args, **kwargs):
