1e39824e8acbf16902532c324ab7dd80ccb1f5ab
Kitaroo <95475183+Kitaroo@users.noreply.github.com>
2021-12-08 22:28:00 +0700

    Update nsfw_sql.py

--- SkyzuRobot/modules/sql/nsfw_sql.py
+++ SkyzuRobot/modules/sql/nsfw_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String
-from SkyzuRobot.modules.sql import BASE, SESSION
+from KyyRobot.modules.sql import BASE, SESSION
 #   |----------------------------------|
 #   |  Test Module by @EverythingSuckz |
 #   |        Kang with Credits         |
