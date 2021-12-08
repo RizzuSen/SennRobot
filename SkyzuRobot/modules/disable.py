c7d0264b6fe2001b6e410023cba5165c49420979
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-08 21:57:04 +0700

    Update disable.py

--- SkyzuRobot/modules/disable.py
+++ SkyzuRobot/modules/disable.py
@@ -22,12 +22,12 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from SkyzuRobot.modules.helper_funcs.chat_status import (
+    from KyyRobot.modules.helper_funcs.chat_status import (
         connection_status,
         is_user_admin,
         user_admin,
     )
-    from SkyzuRobot.modules.sql import disable_sql as sql
+    from KyyRobot.modules.sql import disable_sql as sql
 
     DISABLE_CMDS = []
     DISABLE_OTHER = []
