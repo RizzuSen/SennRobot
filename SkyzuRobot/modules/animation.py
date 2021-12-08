4a76dc3e23abf7168715d143d426b9f8e729d165
Sayonara <89360980+krisnadiwangga@users.noreply.github.com>
2021-12-08 20:43:06 +0700

    Update animation.py

--- SkyzuRobot/modules/animation.py
+++ SkyzuRobot/modules/animation.py
@@ -4,9 +4,9 @@ from typing import List
 from telegram import Update
 from telegram.ext import run_async, CallbackContext
 
-from SkyzuRobot import dispatcher
-from SkyzuRobot.modules.disable import DisableAbleCommandHandler
-from SkyzuRobot.modules.helper_funcs.chat_status import user_admin
+from KyyRobot import dispatcher
+from KyyRobot.modules.disable import DisableAbleCommandHandler
+from KyyRobot.modules.helper_funcs.chat_status import user_admin
 
 # sleep how many times after each edit in 'love'
 EDIT_SLEEP = 1
