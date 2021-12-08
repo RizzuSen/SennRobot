83e61cf6412037d77c2b20baf1c4493e0c707e7f
Sayonara <89360980+krisnadiwangga@users.noreply.github.com>
2021-12-08 22:54:22 +0700

    Update weather.py

--- SkyzuRobot/modules/weather.py
+++ SkyzuRobot/modules/weather.py
@@ -8,10 +8,10 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from SkyzuRobot import *
+from KyyRobot import *
 
-from SkyzuRobot import telethn as tbot
-from SkyzuRobot.events import register
+from KyyRobot import telethn as tbot
+from KyyRobot.events import register
 
 
 async def is_register_admin(chat, user):
