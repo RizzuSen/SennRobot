a9946bc4660b52ea3d12cd18a848b2fb6d794663
Sayonara <89360980+krisnadiwangga@users.noreply.github.com>
2021-12-08 21:46:03 +0700

    Update heroku.py

--- SkyzuRobot/modules/heroku.py
+++ SkyzuRobot/modules/heroku.py
@@ -5,8 +5,8 @@ import os
 import heroku3
 import requests
 
-from SkyzuRobot import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
-from SkyzuRobot.events import register
+from KyyRobot import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
+from KyyRobot.events import register
 
 heroku_api = "https://api.heroku.com"
 Heroku = heroku3.from_key(HEROKU_API_KEY)
