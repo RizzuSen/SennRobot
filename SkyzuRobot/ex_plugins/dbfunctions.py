8cc4e5f9502e395c4451b85e3000fe77b374cb08
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-08 20:19:59 +0700

    Update dbfunctions.py

--- SkyzuRobot/ex_plugins/dbfunctions.py
+++ SkyzuRobot/ex_plugins/dbfunctions.py
@@ -2,7 +2,7 @@ import codecs
 import pickle
 from typing import Dict, List, Union
 
-from SkyzuRobot.mongo import db
+from KyyRobot.mongo import db
 
 # SOME THINGS ARE FUCKED UP HERE, LIKE TOGGLEABLES HAVE THEIR OWN COLLECTION
 # (SHOULD FIX IT WITH SOMETHING LIKE TOGGLEDB), MOST OF THE CODE IS BAD AF
