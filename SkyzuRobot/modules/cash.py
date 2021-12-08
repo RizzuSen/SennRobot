2576824ca7aae611998ada69d7cf7bcd3c20c273
Sayonara <89360980+krisnadiwangga@users.noreply.github.com>
2021-12-08 21:03:08 +0700

    Update cash.py

--- SkyzuRobot/modules/cash.py
+++ SkyzuRobot/modules/cash.py
@@ -3,7 +3,7 @@ import requests
 from telegram import Bot, Update
 from telegram.ext import CommandHandler, run_async
 
-from SkyzuRobot import dispatcher, CASH_API_KEY
+from KyyRobot import dispatcher, CASH_API_KEY
 
 
 def convert(bot: Bot, update: Update):
