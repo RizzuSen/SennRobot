8f69ed74476b7c911d36bc0f6308be697470968f
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-09 03:07:09 +0700

    Update msg_types.py

--- SkyzuRobot/modules/helper_funcs/msg_types.py
+++ SkyzuRobot/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from SkyzuRobot.modules.helper_funcs.string_handling import button_markdown_parser
+from KyyRobot.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
