fb0bb6fd157618e1fb1af7e9b6183ae6813dcdfd
Sayonara <89360980+krisnadiwangga@users.noreply.github.com>
2021-12-08 21:10:00 +0700

    Update country.py

--- SkyzuRobot/modules/country.py
+++ SkyzuRobot/modules/country.py
@@ -5,8 +5,8 @@ from telethon.utils import get_input_location
 import flag
 import html, os
 from countryinfo import CountryInfo
-from SkyzuRobot import telethn as borg
-from SkyzuRobot.events import register
+from KyyRobot import telethn as borg
+from KyyRobot.events import register
 
 
 @register(pattern="^/country (.*)")
