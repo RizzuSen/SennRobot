94cb21589e8856940df9e102f7e407236cda7b96
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-09 03:18:24 +0700

    Update extraction.py

--- SkyzuRobot/modules/helper_funcs/extraction.py
+++ SkyzuRobot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from SkyzuRobot import LOGGER
-from SkyzuRobot.modules.users import get_user_id
+from KyyRobot import LOGGER
+from KyyRobot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
