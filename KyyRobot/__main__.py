e26c188c9ec57e16e9e8aee2e36545ca5332fb28
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-09 07:38:43 +0700

    Update __main__.py

--- KyyRobot/__main__.py
+++ KyyRobot/__main__.py
@@ -134,7 +134,7 @@ CHAT_SETTINGS = {}
 USER_SETTINGS = {}
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("SkyzuRobot.modules." + module_name)
+    imported_module = importlib.import_module("KyyRobot.modules." + module_name)
     if not hasattr(imported_module, "__mod_name__"):
         imported_module.__mod_name__ = imported_module.__name__
 
