0ae6345e9c780c2ee31ebec4d71b4315e16a1f91
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-10 13:09:02 +0700

    Update alive.py

--- KyyRobot/modules/alive.py
+++ KyyRobot/modules/alive.py
@@ -15,10 +15,10 @@ PHOTO = "https://telegra.ph/file/d1b37552917a932acf672.jpg"
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Emiko Robot.** \n\n"
   TEXT += "✘ **I'm Working Properly** \n\n"
-  TEXT += f"✘ **My Master : [kyy-ex](https://t.me/IDnyaKosong)** \n\n"
+  TEXT += f"✘ **My Master : [Kyy](https://t.me/IDnyaKosong)** \n\n"
   TEXT += f"✘ **Library Version :** `{telever}` \n\n"
   TEXT += f"✘ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"✘ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Iam Working Now🔥 **"
-  BUTTON = [[Button.url("Help", "https://t.me/Nastymusiicbot?start=help"), Button.url("Support", "https://t.me/NastySupport")]]
+  BUTTON = [[Button.url("Help", "https://t.me/Nastymusiicbot?start=help"), Button.url("Support", "https://t.me/NastySupportt")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
