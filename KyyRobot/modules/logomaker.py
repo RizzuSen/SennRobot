0ba7e07d288ceab5cb9424b133e1431304b35d95
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-15 20:56:07 +0700

    logo: fixed

--- KyyRobot/modules/logomaker.py
+++ KyyRobot/modules/logomaker.py
@@ -1,53 +1,19 @@
 import os
 import random
 import glob
+
 from PIL import Image, ImageDraw, ImageFont
 from telethon.tl.types import InputMessagesFilterPhotos
 from KyyRobot.events import register
-from KyyRobot import telethn as tbot, ubot2
-
-
-def mediainfo(media):
-    xx = str((str(media)).split("(", maxsplit=1)[0])
-    m = ""
-    if xx == "MessageMediaDocument":
-        mim = media.document.mime_type
-        if mim == "application/x-tgsticker":
-            m = "sticker animated"
-        elif "image" in mim:
-            if mim == "image/webp":
-                m = "sticker"
-            elif mim == "image/gif":
-                m = "gif as doc"
-            else:
-                m = "pic as doc"
-        elif "video" in mim:
-            if "DocumentAttributeAnimated" in str(media):
-                m = "gif"
-            elif "DocumentAttributeVideo" in str(media):
-                i = str(media.document.attributes[0])
-                if "supports_streaming=True" in i:
-                    m = "video"
-                m = "video as doc"
-            else:
-                m = "video"
-        elif "audio" in mim:
-            m = "audio"
-        else:
-            m = "document"
-    elif xx == "MessageMediaPhoto":
-        m = "pic"
-    elif xx == "MessageMediaWebPage":
-        m = "web"
-    return m
+from KyyRobot import telethn as tbot, ubot
 
 
 @register(pattern="^/logo ?(.*)")
 async def logo_gen(event):
-    xx = await event.reply("`Preparing your logo...`")
+    xx = await event.reply("`Processing...`")
     name = event.pattern_match.group(1)
     if not name:
-        await xx.edit("`Provide some text to draw!\nExample: /logo <your name>!`")
+        await xx.edit("`Enter text to create a logo\nExample: /logo <your name>!`")
         return
     bg_, font_ = "", ""
     if event.reply_to_msg_id:
@@ -62,24 +28,24 @@ async def logo_gen(event):
                 bg_ = await temp.download_media()
     else:
         pics = []
-        async for i in ubot2.iter_messages(
-            "@KenLogopack", filter=InputMessagesFilterPhotos
+        async for i in ubot.iter_message(
+            "@NatsunagiLogos", filter=InputMessagesFilterPhotos
         ):
             pics.append(i)
         id_ = random.choice(pics)
         bg_ = await id_.download_media()
-        fpath_ = glob.glob("./SkyzuRobot/resources/fonts/*")
+        fpath_ = glob.glob("./KyyRobot/resources/*")
         font_ = random.choice(fpath_)
     if not bg_:
         pics = []
-        async for i in ubot2.iter_messages(
-            "@KenLogopack", filter=InputMessagesFilterPhotos
+        async for i in ubot.iter_messages(
+            "@NatsunagiLogos", filter=InputMessagesFilterPhotos
         ):
             pics.append(i)
         id_ = random.choice(pics)
         bg_ = await id_.download_media()
     if not font_:
-        fpath_ = glob.glob("./SkyzuRobot/resources/fonts/*")
+        fpath_ = glob.glob("./KyyRobot/resources/*")
         font_ = random.choice(fpath_)
     if len(name) <= 8:
         fnt_size = 120
@@ -104,118 +70,25 @@ async def logo_gen(event):
     )
     x = (image_width - w) / 2
     y = (image_height - h) / 2
-    draw.text((x, y), name, font=font, fill="white",
-              stroke_width=strke, stroke_fill="black")
-    flnme = f"logo.png"
-    img.save(flnme, "png")
-    await xx.edit("`Uploading`")
-    if os.path.exists(flnme):
-        await tbot.send_file(
-            event.chat_id,
-            file=flnme,
-            caption="Logo by [Skyzu Robot](https://t.me/SkyzuRobot)",
-            force_document=False,
-        )
-        os.remove(flnme)
-        await xx.delete()
-    if os.path.exists(bg_):
-        os.remove(bg_) 
-    if os.path.exists(font_):
-        if not font_.startswith("./SkyzuRobot/resources/fonts"):
-            os.remove(font_)
-
-
-@register(pattern="^/wlogo ?(.*)")
-async def logo_(event):
-    xx = await event.reply("`Preparing your logo...`")
-    name = event.pattern_match.group(1)
-    if not name:
-        await xx.edit("`Provide some text to draw!\nExample: /wlogo <your name>!`")
-        return
-    bg_, font_ = "", ""
-    if event.reply_to_msg_id:
-        temp = await event.get_reply_message()
-        if temp.media:
-            if hasattr(temp.media, "document"):
-                if "font" in temp.file.mime_type:
-                    font_ = await temp.download_media()
-                elif (".ttf" in temp.file.name) or (".otf" in temp.file.name):
-                    font_ = await temp.download_media()
-            elif "pic" in mediainfo(temp.media):
-                bg_ = await temp.download_media()
-    else:
-        pics = []
-        async for i in ubot2.iter_messages(
-            "@kenlogopack", filter=InputMessagesFilterPhotos
-        ):
-            pics.append(i)
-        id_ = random.choice(pics)
-        bg_ = await id_.download_media()
-        fpath_ = glob.glob("./SkyzuRobot/resources/fonts/*")
-        font_ = random.choice(fpath_)
-    if not bg_:
-        pics = []
-        async for i in ubot2.iter_messages(
-            "@kenlogopack", filter=InputMessagesFilterPhotos
-        ):
-            pics.append(i)
-        id_ = random.choice(pics)
-        bg_ = await id_.download_media()
-    if not font_:
-        fpath_ = glob.glob("./SkyzuRobot/resources/fonts/*")
-        font_ = random.choice(fpath_)
-    if len(name) <= 8:
-        fnt_size = 105
-        strke = 8
-    elif len(name) >= 9:
-        fnt_size = 50
-        strke = 4
-    else:
-        fnt_size = 95
-        strke = 13
-    img = Image.open(bg_)
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(font_, fnt_size)
-    w, h = draw.textsize(name, font=font)
-    h += int(h * 0.21)
-    image_width, image_height = img.size
     draw.text(
-        ((image_width - w) / 2, (image_height - h) / 2),
-        name,
-        font=font,
-        fill=(255, 255, 255),
+        (x, y), name, font=font, fill="white", stroke_width=strke, stroke_fill="black"
     )
-    x = (image_width - w) / 2
-    y = (image_height - h) / 2
-    draw.text((x, y), name, font=font, fill="white",
-              stroke_width=strke, stroke_fill="black")
-    flnme = f"logo.png"
+    flnme = f"KyyRobot.png"
     img.save(flnme, "png")
-    await xx.edit("`Uploading`")
+    await xx.edit("`Done!`")
     if os.path.exists(flnme):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Skyzu Robot](https://t.me/SkyzuRobot)",
+            caption="Logo by [KyyRobot](https://t.me/Nastymusiicbot)",
             force_document=False,
         )
         os.remove(flnme)
         await xx.delete()
     if os.path.exists(bg_):
-        os.remove(bg_) 
-    if os.path.exists(font_):
-        if not font_.startswith("./SkyzuRobot/resources/fonts"):
-            os.remove(font_)
-
-
-__mod_name__ = "Logomaker"
-
-__help__ = """ This is help menu for logomaker
-
-❂ /logo <text/name> - Create a logo with random view.
-❂ /wlogo <text/name> - Create a logo with wide view only.
-
- Image Editor :
-
-❂  /edit <reply photo> - to edit image.
-"""
+        os.remove(bg_)
+    if (
+        os.path.exists(font_)
+        and not font_.startswith("./KyyRobot/resources")
+    ):
+        os.remove(font_)
