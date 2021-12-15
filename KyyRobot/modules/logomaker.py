2b9efa176db0f9deee2e570e5069eae479f9a661
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-15 21:05:53 +0700

    perfecting

--- KyyRobot/modules/logomaker.py
+++ KyyRobot/modules/logomaker.py
@@ -34,7 +34,7 @@ async def logo_gen(event):
             pics.append(i)
         id_ = random.choice(pics)
         bg_ = await id_.download_media()
-        fpath_ = glob.glob("./KyyRobot/resources/*")
+        fpath_ = glob.glob("./KyyRobot/resources/fonts/*")
         font_ = random.choice(fpath_)
     if not bg_:
         pics = []
@@ -45,7 +45,7 @@ async def logo_gen(event):
         id_ = random.choice(pics)
         bg_ = await id_.download_media()
     if not font_:
-        fpath_ = glob.glob("./KyyRobot/resources/*")
+        fpath_ = glob.glob("./KyyRobot/resources/fonts/*")
         font_ = random.choice(fpath_)
     if len(name) <= 8:
         fnt_size = 120
