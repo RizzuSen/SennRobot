242f600f3dda59879ffe4a3e2371096a2a0390d1
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2021-12-28 19:41:24 +0700

    Update logomaker.py

--- KyyRobot/modules/logomaker.py
+++ KyyRobot/modules/logomaker.py
@@ -63,7 +63,7 @@ async def logo_gen(event):
     else:
         pics = []
         async for i in ubot2.iter_messages(
-            "@PrimeLogoPack", filter=InputMessagesFilterPhotos
+            "@Nastylogo", filter=InputMessagesFilterPhotos
         ):
             pics.append(i)
         id_ = random.choice(pics)
@@ -73,7 +73,7 @@ async def logo_gen(event):
     if not bg_:
         pics = []
         async for i in ubot2.iter_messages(
-            "@PrimeLogopack", filter=InputMessagesFilterPhotos
+            "@Nastylogo", filter=InputMessagesFilterPhotos
         ):
             pics.append(i)
         id_ = random.choice(pics)
@@ -146,7 +146,7 @@ async def logo_(event):
     else:
         pics = []
         async for i in ubot2.iter_messages(
-            "@Primelogopack", filter=InputMessagesFilterPhotos
+            "@Nastylogo", filter=InputMessagesFilterPhotos
         ):
             pics.append(i)
         id_ = random.choice(pics)
@@ -156,7 +156,7 @@ async def logo_(event):
     if not bg_:
         pics = []
         async for i in ubot2.iter_messages(
-            "@Primelogopack", filter=InputMessagesFilterPhotos
+            "@Nastylogo", filter=InputMessagesFilterPhotos
         ):
             pics.append(i)
         id_ = random.choice(pics)
