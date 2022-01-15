a7b4f3f391c9e7bbc2780c51395d88617efac1e8
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-01-15 13:55:35 +0700

    mmk

--- KyyRobot/modules/memify.py
+++ KyyRobot/modules/memify.py
@@ -39,7 +39,7 @@ async def drawText(image_path, text):
     if os.name == "nt":
         fnt = "ariel.ttf"
     else:
-        fnt = "./SkyzuRobot/resources/default.ttf"
+        fnt = "./KyyRobot/resources/default.ttf"
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
     if ";" in text:
         upper_text, lower_text = text.split(";")
