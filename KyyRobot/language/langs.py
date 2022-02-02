36cce8fb2e61be2610e01825d59d8ec3dde3832a
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-02-03 00:21:01 +0700

    ketinggalan

--- KyyRobot/language/langs.py
+++ KyyRobot/language/langs.py
@@ -23,7 +23,7 @@ class Language:
             if filename.endswith(".yaml"):
                 language_name = filename[:-5]
                 self.languages[language_name] = yaml.safe_load(
-                    open(r"./SeiraRobot/language/" + filename, encoding="utf8")
+                    open(r"./KyyRobot/language/" + filename, encoding="utf8")
                 )
 
     def get_languages(self) -> Dict:
